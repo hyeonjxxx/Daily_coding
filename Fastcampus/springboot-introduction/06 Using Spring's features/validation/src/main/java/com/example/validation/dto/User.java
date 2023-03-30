a8b12577ed598b66a42efaa120bafb53be98f450
hyeon <hyeonjxxx@gmail.com>
2023-03-30 23:24:58 +0900

    [230330] Spring boot - Custom Validation 실습
    1. AssertTrue/False 와 같은 method지정을 통해서 Custom Logic 적용가능 (단, 재사용 불가)
    2. ContraintValidator를 적용하여 재사용이 가능한 Custom Logic 적용가능 >> 어노테이션을 만들어주기

--- Fastcampus/springboot-introduction/06 Using Spring's features/validation/src/main/java/com/example/validation/dto/User.java
+++ Fastcampus/springboot-introduction/06 Using Spring's features/validation/src/main/java/com/example/validation/dto/User.java
@@ -1,17 +1,31 @@
 package com.example.validation.dto;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.Pattern;
+
+import com.example.validation.annotation.YearMonth;
+
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 public class User {
 
+    @NotBlank
     private String name;
 
+    @Max(value = 90)
     private int age;
-    @Email
+
     private String email;
-    @Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$", message = "핸드폰 번호의 양식과 맞지않습니다. 01x-xxx(x)-xxxx")
-    private  String phoneNumber;
+//    @Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$", message = "핸드폰 번호의 양식과 맞지않습니다. 01x-xxx(x)-xxxx")
+//    private  String phoneNumber;
+
+//    @YearMonth
+    private String reqYearMonth;    // yyyyMM
+
+    @Valid  // 클래스안에 다른 객체가 있고 object 형태면 반드시 @Valid라고 해주어야 검사가 됨
+    private List<Car> cars;
 
     public String getName() {
         return name;
@@ -37,12 +51,20 @@ public class User {
         this.email = email;
     }
 
-    public String getPhoneNumber() {
-        return phoneNumber;
+    public String getReqYearMonth() {
+        return reqYearMonth;
     }
 
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
+    public void setReqYearMonth(String reqYearMonth) {
+        this.reqYearMonth = reqYearMonth;
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    public void setCars(List<Car> cars) {
+        this.cars = cars;
     }
 
     @Override
@@ -51,7 +73,24 @@ public class User {
                 "name='" + name + '\'' +
                 ", age=" + age +
                 ", email='" + email + '\'' +
-                ", phoneNumber='" + phoneNumber + '\'' +
+                ", reqYearMonth='" + reqYearMonth + '\'' +
+                ", cars=" + cars +
                 '}';
     }
+
+    /*
+    // @AssertTrue 사용시  is라는 키워드로 메소드가 시작해야 한다.
+    // 재활용이 불가능 >> 중복코드가 많아짐 >> 어노테이션을 만들어주는 방법
+    @AssertTrue(message = "yyyymm의 형식에 맞지 않습니다.")
+    public boolean isReqYearMonthValidation(){
+        try {
+            LocalDate localDate = LocalDate.parse(getReqYearMonth() + "01", DateTimeFormatter.ofPattern("yyyyMMdd"));
+        }catch (Exception e){
+            return false;
+        }
+
+        return true;
+    }
+    */
+
 }
