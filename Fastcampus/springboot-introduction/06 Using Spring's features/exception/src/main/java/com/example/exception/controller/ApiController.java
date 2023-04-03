5d10ce2af0b27de5a1870f02ef06b8990ed31102
hyeon <hyeonjxxx@gmail.com>
2023-04-03 23:52:39 +0900

    [230403] Spring boot - Validation과 Exception 활용한 예외처리 실습

--- Fastcampus/springboot-introduction/06 Using Spring's features/exception/src/main/java/com/example/exception/controller/ApiController.java
+++ Fastcampus/springboot-introduction/06 Using Spring's features/exception/src/main/java/com/example/exception/controller/ApiController.java
@@ -3,17 +3,28 @@ package com.example.exception.controller;
 import com.example.exception.dto.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @RestController
 @RequestMapping("/api/user")
+@Validated
 public class ApiController {
 
     @GetMapping ("")
-    public User get(@RequestParam(required = false) String name, @RequestParam(required = false) Integer age){
+    public User get(
+            @Size(min = 2)
+            @RequestParam String name,
+
+            @NotNull
+            @Min(1)
+            @RequestParam Integer age){
         User user =new User();
         user.setName(name);
         user.setAge(age);
@@ -30,11 +41,12 @@ public class ApiController {
 
     }
 
-//    해당 exception은 이 클래스내에서만 적용
-    @ExceptionHandler(value = MethodArgumentNotValidException.class)
-    public ResponseEntity MethodArgumentNotValidException(MethodArgumentNotValidException e){
-        System.out.println(" ----api controller---- ");
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
-    }
+////    해당 exception은 이 클래스내에서만 적용
+//    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+//    public ResponseEntity MethodArgumentNotValidException(MethodArgumentNotValidException e){
+//        System.out.println(" ----api controller---- ");
+//        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+//    }
+
 
 }
